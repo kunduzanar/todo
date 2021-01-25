9f52260dd0f2c9f85be4cc6cecc67904f262ff0b
kunduzanar <kunduzgalaxy@gmail.com>
2021-01-26 02:13:44 +0600

    added static and media urls and root

--- todo/urls.py
+++ todo/urls.py
@@ -17,10 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, second, third
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name = "home"),
     path('test/', test, name = "test"),
     path('test2/', second, name= "second" ),
     path('test3/', third ),
-]
+]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
