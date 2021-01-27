4c3a853a65620e1b4a9bf066b08bf40b5c6cafba
kunduzanar <kunduzgalaxy@gmail.com>
2021-01-27 15:11:12 +0600

    icon managing

--- main/models.py
+++ main/models.py
@@ -6,3 +6,15 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add = True)
     is_done = models.BooleanField(default = False)
     is_favorites = models.BooleanField(default = False)
+   
+class BookModel(models.Model):
+    title = models.CharField(max_length=50)
+    subtititle = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    price = models.PositiveIntegerField(max_length=50)
+    genre = models.CharField(max_length = 250)
+    author = models.CharField(max_length = 300)
+    year = models.DateTimeField()
+    date = models.DateTimeField( )
+
+    
